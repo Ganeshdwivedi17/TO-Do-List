3f12dcb520ace7a4a567164add9b324307fabb85
Ganeshdwivedi17 <139634932+Ganeshdwivedi17@users.noreply.github.com>
2023-07-15 19:38:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+document.getElementById("add").addEventListener("click",AddTask);
+
+var inputBox = document.getElementById("input-box");
+
+var listContainer = document.getElementById("list-container");
+
+function AddTask(){
+if(document.getElementById("input-box").value === ''){
+alert("You must write something");
+}
+else{
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+
+}
+inputBox.value="";
+saveData();
+}
+listContainer.addEventListener('click',function(e){
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+});
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+function showtask(){
+
+listContainer.innerHTML = localStorage.getItem("data");
+}
+showtask();
